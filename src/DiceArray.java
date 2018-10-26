241ffd0d6c2ac8954a26a0e903bb6143911c999d
tanyakalianda <42977166+tanyakalianda@users.noreply.github.com>
2018-10-26 13:39:16 -0400

    Add files via upload

---
+++ src/DiceArray.java
@@ -0,0 +1,18 @@
+
+public class DiceArray
+{
+
+	public static void main(String[] args)
+	{
+		Dice dice1 = new Dice();
+		Dice dice2 = new Dice();
+		
+		for (int i = 0, i<count.length; i++)
+		{
+			int count[] = new int[13];
+		}
+		
+
+	}
+
+}
